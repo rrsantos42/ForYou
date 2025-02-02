a9fcc7f15ab205d809e895f20ad1ded50417885e
ctw03397 <ricardo.ar.rodrigues@ctw.bmwgroup.com>
2025-02-02 23:25:23 +0000

    FirstPush

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoveMessage from "./Comp/LoveMessage";
+
+function App() {
+  return <LoveMessage />;
+}
+
+export default App;
---
+++ src/Comp/LoveMessage.tsx
@@ -0,0 +1,6 @@
+
+function LoveMessage() {
+    return <h1 style={{ textAlign: "center", color: "red" }}>I love you ❤️</h1>;
+}
+
+export default LoveMessage;
